5324fbdcdf8d292187f1d1003b282c476af983d3
walle <zhangqi2610@gmail.com>
2018-10-26 15:11:58 +0800

    remove exception

--- spring-security-jwt-auth/src/main/java/me/lecoding/springlearning/jwtauth/jwt/JWTAuthConverter.java
+++ spring-security-jwt-auth/src/main/java/me/lecoding/springlearning/jwtauth/jwt/JWTAuthConverter.java
@@ -1,7 +1,5 @@
 package me.lecoding.springlearning.jwtauth.jwt;
 
-import me.lecoding.springlearning.jwtauth.exception.CustomException;
-import me.lecoding.springlearning.jwtauth.exception.ErrorInfoCode;
 import org.springframework.http.HttpHeaders;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.server.ServerWebExchange;
@@ -16,7 +14,6 @@ public class JWTAuthConverter implements Function<ServerWebExchange,Mono<Authent
     public Mono<Authentication> apply(ServerWebExchange serverWebExchange) {
         return Mono.justOrEmpty(serverWebExchange)
                 .filter(w->w.getRequest().getHeaders().containsKey(HttpHeaders.AUTHORIZATION))
-                .switchIfEmpty(Mono.error(new CustomException(ErrorInfoCode.NEED_AUTHENTICATION)))
                 .map(JWTUtils::getAuthorizationPayload)
                 .filter(Objects::nonNull)
                 .filter(JWTUtils.matchBearerLength())
